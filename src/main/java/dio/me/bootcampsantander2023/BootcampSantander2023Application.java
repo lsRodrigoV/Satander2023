5d7b9f25cdf12aadbdd6d45076c9b4c5804c7025
Rodrigo Lopes <rodrigovlskz@gmail.com>
2023-10-21 20:09:12 -0300

    first commit

---
+++ src/main/java/dio/me/bootcampsantander2023/BootcampSantander2023Application.java
@@ -0,0 +1,13 @@
+package dio.me.bootcampsantander2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantander2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantander2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/bootcampsantander2023/BootcampSantander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.bootcampsantander2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
